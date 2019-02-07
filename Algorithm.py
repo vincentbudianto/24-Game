2d1ceff4612ecb43128821548f4fb7c66e8d2cbe
Oksidian Tafly <oksitafly@gmail.com>
2019-02-07 18:46:04 +0700

    semoga fix wkwkwkwkwwkwkwkwkwkwkkwwkkw

--- Algorithm.py
+++ Algorithm.py
@@ -17,6 +17,7 @@ suit=["S","D","C","H"]
 #fungsi tambahan
 def initiatedeck(): #inisiasi dek
     global deck
+    deck=[]
     for i in range(1,14):
         for fn in suit:
             if (i==1 or i>10):
@@ -73,10 +74,35 @@ def compare24(num,exp): #menghitung bobot skor
     skor=skor-abs(24-num)
     return skor
 
+def kurung(st): #menambahkan kurung pada st
+    global totalscore
+    b=[]
+    idx=[]
+    i=0
+    while (i<len(st)):
+        ch=st[i]
+        if (ch=='+') or (ch=='-'):
+            b.append(False)
+            idx.append(i)
+        elif (ch=='/') or (ch=='*'):
+            b.append(True)
+            idx.append(i)
+        i+=1
+    if (not b[1] and b[2]):
+        return "("+st[0:idx[2]]+")"+st[idx[2]:]
+        totalscore-=1
+    elif (not b[0] and b[1]):
+        return "("+st[0:idx[1]]+")"+st[idx[1]:]
+        totalscore-=1
+    else:
+        return st
+
+
 def mainwithargv(): #main program
     global List
     global totalscore
 
+    List=[]
     with open(sys.argv[1],"r") as f: #baca teks dari hans
         #read first line
         for value in next(f).split():
@@ -96,9 +122,11 @@ def mainwithcards():
     global deck
     global List
     global totalscore
+    List=[]
     take4()
     Listprocess()
-    #print(List[0].E)
+    List[0].E=kurung(List[0].E)
+    #print(s)
     totalscore-=abs(24-List[0].N)
     #print("totalscore="+str(totalscore))
 
@@ -109,12 +137,14 @@ if (len(sys.argv)>1):
 else:
     n24=0
     scoring=0
-    for i in range (1,1000):
+    for i in range (0,1000):
         totalscore=0
         initiatedeck()
         mainwithcards()
         if List[0].N == 24:
             n24+=1
+        print(List[0].E)
+        print(totalscore)
         scoring=scoring+totalscore
     print(n24)
-    print(scoring)
+    print(scoring/1000)
