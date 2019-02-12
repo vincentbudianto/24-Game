4350c16d2df40e17f10bee874c4c87087c5b8ad6
Oksidian Tafly <oksitafly@gmail.com>
2019-02-12 18:14:07 +0700

    final fix 5

--- Algorithm.py
+++ Algorithm.py
@@ -5,41 +5,6 @@ from functions import *
 #inisiasi global variabel
 deck=[]
 
-def take4():  #mengambil 4 kartu dari deck
-	global deck
-	global List
-
-	for i in range (1,5):
-		x=random.randint(0,len(deck)-1)
-		c=deck.pop(x)
-		List.append(c.c2n())
-
-def Listprocess():
-	global List
-	global totalscore
-
-	quickSort(List,0,len(List)-1)
-	while (len(List)>1):
-		N1=List.pop(0)
-		N2=List.pop(0)
-		if (N1<=24):
-			if (compare24(N1+N2,'+')>compare24(N1*N2,'*')):
-				N3=N1.operate('+',N2)
-				totalscore+=5
-			else:
-				N3=N1.operate('*',N2)
-				totalscore+=3
-		else:
-			if (compare24(N1-N2,'-')>compare24(N1/N2,'/')):
-				N3=N1.operate('-',N2)
-				totalscore+=4
-			else:
-				N3=N1.operate('/',N2)
-				totalscore+=2
-		List.append(N3)
-		quickSort(List,0,len(List)-1)
-
-
 def mainwithargv(): #main program
     global List
     global totalscore
@@ -56,7 +21,7 @@ def mainwithargv(): #main program
     fx = open(sys.argv[2],"w")#tulis ke file
     fx.write(List[0].E)
     fx.write("\nTotal Skor: ")
-    fx.write(str(totalscore))
+    fx.write(str(int(totalscore)))
 
 if (len(sys.argv) == 3):
 	mainwithargv()
