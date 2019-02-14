b0073af60b68ab4494acef73b66979fb3feb8087
vincent VB <42698353+vincentbudianto@users.noreply.github.com>
2019-02-15 00:33:04 +0700

    Add files via upload

--- GUI Output.py
+++ GUI Output.py
@@ -17,14 +17,18 @@ from kivymd.dialog import MDDialog
 from kivymd.label import MDLabel
 from kivy.properties import ObjectProperty
 from kivy.lang.builder import Builder
+from kivy.animation import Animation
 from kivymd.theming import ThemeManager
 
 main_widget_kv = '''
 #:import hex kivy.utils.get_color_from_hex
 #:import ThemeManager kivymd.theming.ThemeManager
 #:import MDTextField kivymd.textfields.MDTextField
+#:import FadeTransition kivy.uix.screenmanager.FadeTransition
+
 ScreenManager:
 	id: scr_mngr
+	transition: FadeTransition()
 	Screen1:
 		name: 'Screen1'
 		canvas.before:
@@ -33,40 +37,54 @@ ScreenManager:
 				pos: self.pos
 				size: self.size
 		MDLabel:
+			id: title1
 			font_style: 'Display1'
 			text: "                             #####    #                 #            #   ##########   ########                      #"
 			halign: 'left'
 			size_hint_y: 1.8
+			opacity: 1
 		MDLabel:
+			id: title2
 			font_style: 'Display1'
 			text: "                            #         #   #        #          #       #                     ###                        #   ##########"
 			halign: 'left'
 			size_hint_y: 1.7
+			opacity: 1
 		MDLabel:
+			id: title3
 			font_style: 'Display1'
 			text: "                                       #   #        #       #   #   #                     #                            #                     ##"
 			halign: 'left'
 			size_hint_y: 1.6
+			opacity: 1
 		MDLabel:
+			id: title4
 			font_style: 'Display1'
 			text: "                             ####    #        #                #                      #                         ##                   ##  #"
 			halign: 'left'
 			size_hint_y: 1.5
+			opacity: 1
 		MDLabel:
+			id: title5
 			font_style: 'Display1'
 			text: "                            #            ######            #  #                 #                      ##   #               ##       #"
 			halign: 'left'
 			size_hint_y: 1.4
+			opacity: 1
 		MDLabel:
+			id: title6
 			font_style: 'Display1'
 			text: "                            #                      #             #      #             #                      ##       #         ##        ##  "
 			halign: 'left'
 			size_hint_y: 1.3
+			opacity: 1
 		MDLabel:
+			id: title7
 			font_style: 'Display1'
 			text: "                            #####           #                        #         #                      #              #                       #  "
 			halign: 'left'
 			size_hint_y: 1.2
+			opacity: 1
 		BoxLayout:
 			orientation: 'vertical'
 			size_hint_y: 0.4
@@ -110,7 +128,9 @@ ScreenManager:
 					elevation_normal: 2
 					opposite_colors: True
 					pos_hint: {'center_x': 0.5, 'center_y': 0.4}
-					on_press: app.close()
+					on_press:
+						app.close()
+	
 
 	Screen2:
 		name: 'Screen2'
@@ -165,19 +185,19 @@ ScreenManager:
 		BoxLayout:
 			orientation: 'horizontal'
 			size_hint_y: 0.3
-			pos_hint: {'center_x': 0.5, 'center_y': 0.6}
+			pos: -500, 400
 			Image:
 				id: gbr1
-				source: './Picture/2D.png'
+				source: ''
 			Image:
 				id: gbr2
-				source: './Picture/2H.png'
+				source: ''
 			Image:
 				id: gbr3
-				source: './Picture/2C.png'
+				source: ''
 			Image:
 				id: gbr4
-				source: './Picture/2S.png'
+				source: ''
 		BoxLayout:
 			orientation: 'horizontal'
 			size_hint_y: None
@@ -220,105 +240,116 @@ ScreenManager:
 				valign: 'top'
 				size_hint_y: 1.8
 		BoxLayout:
-			orientation: 'horizontal'
+			orientation: 'vertical'
 			size_hint_y: 0.35
-			center: self.parent.center
-			padding: dp(48)
+			pos: -75, 275
 			spacing: 50
 			BoxLayout:
-				orientation: 'vertical'
-				MDLabel:
-					id: label3
-					font_style: 'Display1'
-					color: hex('#FFEFD5')
-					text: "Hasil       : "
-					halign: 'right'
-					valign: 'top'
-				MDLabel:
-					id: label4
-					font_style: 'Display1'
-					color: hex('#FFEFD5')
-					text: "Poin        : "
-					halign: 'right'
-					valign: 'top'
-				MDLabel:
-					id: label5
-					font_style: 'Display1'
-					color: hex('#FFEFD5')
-					text: "Total Poin  : "
-					halign: 'right'
-					valign: 'top'
-			BoxLayout:
-				orientation: 'vertical'
-				MDLabel:
-					id: hasil
-					font_style: 'Display2'
-					color: hex('#FFEFD5')
-					text: ""
-					halign: 'left'
-					valign: 'top'
-				MDLabel:
-					id: poin
-					font_style: 'Display2'
-					color: hex('#FFEFD5')
-					text: ""
-					halign: 'left'
-					valign: 'top'
-				MDLabel:
-					id: totalpoin
-					font_style: 'Display2'
-					color: hex('#FFEFD5')
-					text: "0"
-					halign: 'left'
-					valign: 'top'
-			BoxLayout:
-				orientation: 'vertical'
+				orientation: 'horizontal'
+				size_hint_y: 0.35
+				center: self.parent.center
+				padding: dp(48)
 				spacing: 50
 				BoxLayout:
-					Button:
-						id: button4
-						text: "NEXT DRAW"
-						background_color: hex('#80ffff')
-						size_hint: 0.3, 1.5
-						elevation_normal: 2
-						opposite_colors: True
-						pos_hint: {'center_x': 0.5, 'center_y': 0.4}
-						on_press:
-							app.nextdraw()
+					orientation: 'vertical'
+					MDLabel:
+						id: label3
+						font_style: 'Display1'
+						color: hex('#FFEFD5')
+						text: "Hasil       : "
+						halign: 'right'
+						valign: 'top'
+						opacity: 0
+					MDLabel:
+						id: label4
+						font_style: 'Display1'
+						color: hex('#FFEFD5')
+						text: "Poin        : "
+						halign: 'right'
+						valign: 'top'
+						opacity: 0
+					MDLabel:
+						id: label5
+						font_style: 'Display1'
+						color: hex('#FFEFD5')
+						text: "Total Poin  : "
+						halign: 'right'
+						valign: 'top'
+						opacity: 0
 				BoxLayout:
-					Button:
-						id: button5
-						text: "NEW GAME"
-						background_color: hex('#33ff33')
-						size_hint: 0.3, 1.5
-						elevation_normal: 2
-						opposite_colors: True
-						pos_hint: {'center_x': 0.5, 'center_y': 0.4}
-						on_press:
-							hasil.text = ""
-							_hasil.text = ""
-							poin.text = ""
-							_poin.text = ""
-							totalpoin.text = "0"
-							pname1.text = "Name : "
-							pname2.text = "Name : "
-							playername.text = ""
-							cardsleft1.text = "Cards Left: "
-							cardsleft2.text = "Cards Left: "
-							_cardsleft.text = ""
-							root.current = 'Screen1'
-							app.newgame()
+					orientation: 'vertical'
+					MDLabel:
+						id: hasil
+						font_style: 'Display2'
+						color: hex('#FFEFD5')
+						text: ""
+						halign: 'left'
+						valign: 'top'
+						opacity: 0
+					MDLabel:
+						id: poin
+						font_style: 'Display2'
+						color: hex('#FFEFD5')
+						text: ""
+						halign: 'left'
+						valign: 'top'
+						opacity: 0
+					MDLabel:
+						id: totalpoin
+						font_style: 'Display2'
+						color: hex('#FFEFD5')
+						text: "0"
+						halign: 'left'
+						valign: 'top'
+						opacity: 0
 				BoxLayout:
-					Button:
-						id: button6
-						text: "EXIT"
-						background_color: hex('#FF0000')
-						size_hint: 0.3, 1.5
-						elevation_normal: 2
-						opposite_colors: True
-						pos_hint: {'center_x': 0.5, 'center_y': 0.4}
-						on_release:
-							app.close()
+					orientation: 'vertical'
+					spacing: 50
+					BoxLayout:
+						Button:
+							id: button4
+							text: "NEXT DRAW"
+							background_color: hex('#80ffff')
+							size_hint: 0.3, 1.5
+							elevation_normal: 2
+							opposite_colors: True
+							pos_hint: {'center_x': 0.5, 'center_y': 0.4}
+							on_press:
+								app.nextdraw()
+					BoxLayout:
+						Button:
+							id: button5
+							text: "NEW GAME"
+							background_color: hex('#33ff33')
+							size_hint: 0.3, 1.5
+							elevation_normal: 2
+							opposite_colors: True
+							pos_hint: {'center_x': 0.5, 'center_y': 0.4}
+							on_press:
+								hasil.text = ""
+								_hasil.text = ""
+								poin.text = ""
+								_poin.text = ""
+								totalpoin.text = "0"
+								pname1.text = "Name : "
+								pname2.text = "Name : "
+								playername.text = ""
+								cardsleft1.text = "Cards Left: "
+								cardsleft2.text = "Cards Left: "
+								_cardsleft.text = ""
+								root.current = 'Screen1'
+								app.newgame()
+					BoxLayout:
+						Button:
+							id: button6
+							text: "EXIT"
+							background_color: hex('#FF0000')
+							size_hint: 0.3, 1.5
+							elevation_normal: 2
+							opposite_colors: True
+							pos_hint: {'center_x': 0.5, 'center_y': 0.4}
+							on_release:
+								app.close()
 		BoxLayout:
 			orientation: 'horizontal'
 			size_hint_y: None
@@ -356,7 +387,7 @@ class main(App):
 	theme_cls = ThemeManager()
 	title = "24 Game"
 	Window.fullscreen = 'auto'
-	
+
 	def build(self):
 		main_widget = Builder.load_string(main_widget_kv)
 		return main_widget
@@ -373,6 +404,25 @@ class main(App):
 			self.root.ids.gbr2.source = str(drawncards[1].FN)
 			self.root.ids.gbr3.source = str(drawncards[2].FN)
 			self.root.ids.gbr4.source = str(drawncards[3].FN)
+			
+			anim1 = Animation(x = 50, y = 400, t = 'linear', duration = 1)
+			anim2 = Animation(x = 400, y = 400, t = 'linear', duration = 1)
+			anim3 = Animation(x = 750, y = 400, t = 'linear', duration = 1)
+			anim4 = Animation(x = 1100, y = 400, t = 'linear', duration = 1)
+			anim1.start(self.root.ids.gbr1)
+			anim2.start(self.root.ids.gbr2)
+			anim3.start(self.root.ids.gbr3)
+			anim4.start(self.root.ids.gbr4)
+			
+			reset1 = Animation(opacity = 0)
+			reset1.start(self.root.ids.title1)
+			reset1.start(self.root.ids.title2)
+			reset1.start(self.root.ids.title3)
+			reset1.start(self.root.ids.title4)
+			reset1.start(self.root.ids.title5)
+			reset1.start(self.root.ids.title6)
+			reset1.start(self.root.ids.title7)
+			
 			self.root.current = 'Screen2'
 		else:
 			self.root.ids.errorcode.opacity = 1
@@ -384,12 +434,26 @@ class main(App):
 		(List,totalscore)=Listprocessf(List,totalscore)
 		(List[0].E,totalscore)=kurung(List[0].E,totalscore)
 		totalscore-=abs(24-List[0].N)
-
+		
+		reset2 = Animation(x = -500, y = 400, t = 'linear', duration = 2)
+		reset2.start(self.root.ids.gbr1)
+		reset2.start(self.root.ids.gbr2)
+		reset2.start(self.root.ids.gbr3)
+		reset2.start(self.root.ids.gbr4)
+		
 		self.root.ids._hasil.text = str(List[0].E)
 		self.root.ids._poin.text = str(totalscore)
 		self.root.ids._cardsleft.text = str(len(deck))
 		self.root.ids.totalpoin.text = str(int(self.root.ids.totalpoin.text) + int(self.root.ids._poin.text))
 
+		animat = Animation(opacity = 1, duration = 1)
+		animat.start(self.root.ids.label3)
+		animat.start(self.root.ids.hasil)
+		animat.start(self.root.ids.label4)
+		animat.start(self.root.ids.poin)
+		animat.start(self.root.ids.label5)
+		animat.start(self.root.ids.totalpoin)
+		
 		self.root.current = 'Screen3'
 
 	def nextdraw(self):
@@ -403,12 +467,29 @@ class main(App):
 			self.root.ids.gbr2.source = str(drawncards[1].FN)
 			self.root.ids.gbr3.source = str(drawncards[2].FN)
 			self.root.ids.gbr4.source = str(drawncards[3].FN)
+			
+			anim1 = Animation(x = 50, y = 400, t = 'linear', duration = 1)
+			anim2 = Animation(x = 400, y = 400, t = 'linear', duration = 1)
+			anim3 = Animation(x = 750, y = 400, t = 'linear', duration = 1)
+			anim4 = Animation(x = 1100, y = 400, t = 'linear', duration = 1)
+			anim1.start(self.root.ids.gbr1)
+			anim2.start(self.root.ids.gbr2)
+			anim3.start(self.root.ids.gbr3)
+			anim4.start(self.root.ids.gbr4)
+			
+			reset3 = Animation(opacity = 0)
+			reset3.start(self.root.ids.label3)
+			reset3.start(self.root.ids.hasil)
+			reset3.start(self.root.ids.label4)
+			reset3.start(self.root.ids.poin)
+			reset3.start(self.root.ids.label5)
+			reset3.start(self.root.ids.totalpoin)
+			
 			self.root.current = 'Screen2'
 		else:
 			content = MDLabel(font_style = 'Body1',
 							  theme_text_color = 'Secondary',
-							  text = "There's no card left. "
-									 "The game is over!\n"
+							  text = "There's no card left. The game is over!\n"
 									 "Please start a new game or exit the game.",
 							  size_hint_y = None,
 							  valign = 'top')
@@ -427,9 +508,34 @@ class main(App):
 		global deck
 
 		deck = initiatedeck()
+		
+		reset4 = Animation(opacity = 0)
+		reset4.start(self.root.ids.label4)
+		reset4.start(self.root.ids.hasil)
+		reset4.start(self.root.ids.label4)
+		reset4.start(self.root.ids.poin)
+		reset4.start(self.root.ids.label5)
+		reset4.start(self.root.ids.totalpoin)
+		
+		anima1 = Animation(opacity = 1, duration = 4)
+		anima2 = Animation(opacity = 1, duration = 4.5)
+		anima3 = Animation(opacity = 1, duration = 5)
+		anima4 = Animation(opacity = 1, duration = 5.5)
+		anima5 = Animation(opacity = 1, duration = 6)
+		anima6 = Animation(opacity = 1, duration = 6.5)
+		anima7 = Animation(opacity = 1, duration = 7)
+		anima1.start(self.root.ids.title1)
+		anima2.start(self.root.ids.title2)
+		anima3.start(self.root.ids.title3)
+		anima4.start(self.root.ids.title4)
+		anima5.start(self.root.ids.title5)
+		anima6.start(self.root.ids.title6)
+		anima7.start(self.root.ids.title7)
 
 if __name__ == '__main__':
 	sound = SoundLoader.load('./Voyage.wav')
+	sound.volume = 0.4
 	sound.loop = True
 	sound.play()
 	main().run()
+	
